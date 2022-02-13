e2487137109ed4f45d78b04113de6cf8e84f8b20
Mohd7979 <99615092+Mohd7979@users.noreply.github.com>
2022-02-14 06:17:52 +0800

    Update PopsWelli.js

--- PopsWelli.js
+++ PopsWelli.js
@@ -6,55 +6,55 @@
 	
 	    var myconfirm = document.getElementById("Welli1")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli2")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli3")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli4")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli5")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli6")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli7")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli8")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli9")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli10")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
 	
 		    var myconfirm = document.getElementById("Welli11")
     myconfirm.addEventListener("click", function(){
-        $confirm("Kulankaan Ma Uusan Bilaaban", "#0B8043")
+        $confirm("Kulankaan Ma Uusan Bilaaban!", "#0B8043")
     });
