9cebd86d9d0beff2b3e3e807e2389ca3ea655797
Mohd7979 <99615092+Mohd7979@users.noreply.github.com>
2022-02-14 08:16:46 +0800

    Add files via upload

---
+++ PopsDhamaad1.js
@@ -0,0 +1,59 @@
+	
+			    var myconfirm = document.getElementById("Dhamaad")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad1")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad2")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad3")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad4")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad5")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad6")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad7")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad8")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad9")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+				    var myconfirm = document.getElementById("Dhamaad10")
+    myconfirm.addEventListener("click", function(){
+        $confirm("Kulankaan Wuu Dhamaade!", "#0B8043")
+    });
+	
+	
+	
+	
